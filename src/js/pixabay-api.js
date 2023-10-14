46b6868fe75a6560084ec9545b3536eeab2b8c6e
Dimch93 <dimch93@ukr.net>
2023-10-14 17:22:30 +0300

    home-11.1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,2 +1,57 @@
-const url = 'https://pixabay.com/api/';
-const api_key = '39933999-f0fa465a086f20cae0ce79cf5';
+// url = 'https://pixabay.com/api/';
+// api_key = '39933999-f0fa465a086f20cae0ce79cf5';
+
+import axios from 'axios';
+
+export default class NewsApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.PER_PAGE = 40;
+  }
+  async fetchGallery() {
+    const axiosOptions = {
+      method: 'get',
+      url: 'https://pixabay.com/api/',
+      params: {
+        key: '39933999-f0fa465a086f20cae0ce79cf5',
+        q: `${this.searchQuery}`,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: `${this.page}`,
+        per_page: `${this.PER_PAGE}`,
+      },
+    };
+    try {
+      const response = await axios(axiosOptions);
+
+      const data = response.data;
+
+      this.incrementPage();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  resetEndOfHits() {
+    this.endOfHits = false;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
